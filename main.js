89d2762d0a6ca8e7295706f4e9425255f6c53344
ebdevStudent <78286626+ebdevStudent@users.noreply.github.com>
2021-01-30 21:25:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function changecolor(){
+document.getElementById("heading1").style.color="green";
+}
+function changecolor2(){
+document.getElementById("heading2").style.color="white";
+}
+function changesize(){
+document.getElementById("heading3").style.fontSize = "xx-large";
+}
+function changebc(){
+document.getElementById("heading4").style.backgroundColor = "aqua";
+}
