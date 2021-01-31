2bd567471b89d5623726bd21a264332525132d8f
dllobuz <dllobuz@gmail.com>
2021-01-31 12:07:37 +0300

    update all

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-function changecolor(){
-document.getElementById("heading1").style.color="green";
+function changecolor2() {
+  document.getElementById("heading2").style.color = "black";
 }
-function changecolor2(){
-document.getElementById("heading2").style.color="white";
+function changesize() {
+  document.getElementById("heading3").style.fontSize = "xx-large";
 }
-function changesize(){
-document.getElementById("heading3").style.fontSize = "xx-large";
+function changebc() {
+  document.getElementById("heading4").style.backgroundColor = "aqua";
 }
-function changebc(){
-document.getElementById("heading4").style.backgroundColor = "aqua";
-}
+
+// alert("Don't worry, I'm just testing the git. Good luck")
